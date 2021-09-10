4aa1debe6c3cf11b100c426f8671349421d4c62d
Simon Fraser <sii_fraser@hotmail.co.uk>
2021-09-10 12:22:26 +0100

    Completed homework

---
+++ controllers/museums_controller.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.museum import Museum
+import repositories.museum_repository as museum_repository
+import repositories.work_repository as work_repository
+
+museums_blueprint = Blueprint("museums", __name__)
+
+# INDEX
+# GET '/museums
+@museums_blueprint.route("/museums")
+def museums():
+    all_museums = museum_repository.select_all()
+    return render_template ("/museums/index.html", museums = all_museums)
+
+# NEW
+# GET '/museums/new'
+
+@museums_blueprint.route("/museums/new", methods=['GET'])
+def new_museum():
+    return render_template ("museums/new.html")
+
+# CREATE
+# POST '/museums'
+@museums_blueprint.route("/museums", methods=['POST'])
+def add_museum():
+    name = request.form['name']
+    address = request.form['address']
+    new_museum = Museum(name, address)
+    museum_repository.save(new_museum)
+    return redirect("/museums")
+
+# SHOW
+# GET '/museums/<id>'
+@museums_blueprint.route("/museums/<id>", methods=['GET'])
+def show_work(id):
+    museum = museum_repository.select(id)
+    return render_template("museums/show.html", museum=museum)
+
+# EDIT
+# GET '/museums/<id>/edit'
+@museums_blueprint.route("/museums/<id>/edit", methods=['GET'])
+def edit_museum(id):
+    museum = museum_repository.select(id)
+    return render_template("/museums/edit.html", museum=museum)
+
+# UPDATE
+# PUT '/museums/<id>'
+@museums_blueprint.route("/museums/<id>", methods=['POST'])
+def update_museum(id):
+    name = request.form['name']
+    address = request.form['address']
+    museum = Museum(name, address, id)
+    museum_repository.update(museum)
+    return redirect('/museums')
+
+# DELETE
+# DELETE '/museums/<id>'
+@museums_blueprint.route("/museums/<id>/delete", methods=['POST'])
+def delete_museum(id):
+    museum_repository.delete(id)
+    return redirect('/museums')
+
+
